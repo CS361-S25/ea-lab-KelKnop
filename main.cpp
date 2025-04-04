28fbcb98babe77a41484519ba073e544c4fba6a7
Kellen Knop <ceylan@Kellens-MacBook-Pro-4.local>
2025-04-04 09:22:30 -0500

    starting on ea

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include "Empirical/include/emp/math/Random.hpp"
 
+
+class Organism {
+  public:
+    double behavior;
+
+    // a constructor that takes a double and sets the instance variable.
+    Organism(double b) : behavior(b) { }
+
+};
+
 int main() {
+
+    emp::vector<Organism> population;
+
+    double b = emp::Random random(1);
+
+    for (int i = 0; i < 100; i++) {
+      population.push_back(Organism(b));
+      cout << "Organism " << i << " behavior: " << population[i].behavior << std::endl;
+    }
 
 }
